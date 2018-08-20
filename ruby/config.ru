dc86868301defcd6cffe391959639ff29f71de83
Eleatzar Colomer <eleatzar.colomer@nodomain.local>
2018-08-20 19:28:09 +0200

    Add Gemfile and Rackup config
    When using OpenShift Source-to-Image (S2I) build strategy, the easiest way to
    run a Ruby web application is using Rack. We could override S2I scripts which
    assemble and configure the runtime environment if we needed though.
    
    So, we add a config.ru file for the rackup command and a Gemfile which
    will install the required Ruby gems (redis, sinatra). And we adapt the
    hello-world.rb to be compatible with config.ru configuration file.
    
    Because the rackup command is set to listen on port 8080 we also update the
    OpenShift template to expose 8080 on the container and set this on the service
    as well.

---
+++ ruby/config.ru
@@ -0,0 +1,3 @@
+require File.expand_path('hello-world', File.dirname(__FILE__))
+
+run HelloWorld
--- ruby/hello-world.rb
+++ ruby/hello-world.rb
@@ -1,10 +1,13 @@
 require 'sinatra'
 require 'redis'
 
-redis = Redis.new(:host => ENV["REDIS_HOST"] || "127.0.0.1" , :port => ENV["REDIS_PORT"] || 6379)
+class HelloWorld < Sinatra::Base
 
-get '/' do
-  redis.ping
-  "Hello World!"
+  redis = Redis.new(:host => ENV["REDIS_HOST"] || "127.0.0.1" , :port => ENV["REDIS_PORT"] || 6379)
+
+  get '/' do
+    redis.ping
+    "Hello World!"
+  end
+
 end
-
